74c1d9704ecc29d3c676a3d277643f797a1532de
bsevgeny@gmail.com <papakita2009>
2016-08-26 18:18:55 +0300

    BFPP implemented

---
+++ src/main/java/spring/quoters/bpp/BeanDefinitionAnalyzerBeanFactoryPostProcessor.java
@@ -0,0 +1,26 @@
+package spring.quoters.bpp;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+
+/**
+ * Created by Evegeny on 26/08/2016.
+ */
+public class BeanDefinitionAnalyzerBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    @SneakyThrows
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        String[] names = beanFactory.getBeanDefinitionNames();
+        for (String name : names) {
+            AbstractBeanDefinition beanDefinition = (AbstractBeanDefinition) beanFactory.getBeanDefinition(name);
+            String methodName = beanDefinition.getDestroyMethodName();
+            if (methodName != null && beanDefinition.isPrototype()) {
+                System.err.println("Warning: you are an Idiot");
+            }
+        }
+    }
+}
